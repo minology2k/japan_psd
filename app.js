17122da8873eb6a6ca1c6aa62633c579a582a3a0
minh2605 <bluewings2605@gmail.com>
2021-10-30 08:11:08 +0700

    Minh_addcomment

--- app.js
+++ app.js
@@ -1,48 +1,45 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelector.bind(document);
 
-const btnToBottom = $('.banner__button');
-const btnToTop = $('.to-header');
-const categorySection = $('#category');
-const destinationSection = $('#destination');
-const tripSection = $('#trip');
+const btnToBottom = $(".banner__button");
+const btnToTop = $(".to-header");
+const categorySection = $("#category");
+const destinationSection = $("#destination");
+const tripSection = $("#trip");
 
-btnToTop.addEventListener('click', function() {
-    console.log(document.body.getBoundingClientRect().top);
-    window.scrollTo(0, 0);
-})
-btnToBottom.addEventListener('click', ()=> {
-    console.log(document.body.scrollHeight);
-    window.scrollTo(0,document.body.scrollHeight);
-})
+btnToTop.addEventListener("click", function () {
+  console.log(document.body.getBoundingClientRect().top);
+  window.scrollTo(0, 0);
+});
+btnToBottom.addEventListener("click", () => {
+  console.log(document.body.scrollHeight);
+  window.scrollTo(0, document.body.scrollHeight);
+});
 
-window.addEventListener('scroll', function() {
-    var screenPosition = window.innerHeight / 2.2;
+window.addEventListener("scroll", function () {
+  var screenPosition = window.innerHeight / 2.2;
 
-    var categoryPosition = categorySection.getBoundingClientRect().top;
-    if(categoryPosition < screenPosition) {
-        categorySection.classList.add('active');
-    }
-    else {
-        categorySection.classList.remove('active');
-    }
+  var categoryPosition = categorySection.getBoundingClientRect().top;
+  if (categoryPosition < screenPosition) {
+    categorySection.classList.add("active");
+  } else {
+    categorySection.classList.remove("active");
+  }
 
-    var destinationPosition = destinationSection.getBoundingClientRect().top;
-    if(destinationPosition < screenPosition) {
-        destinationSection.classList.add('active');
-    }
-    else {
-        destinationSection.classList.remove('active');
-    }
+  var destinationPosition = destinationSection.getBoundingClientRect().top;
+  if (destinationPosition < screenPosition) {
+    destinationSection.classList.add("active");
+  } else {
+    destinationSection.classList.remove("active");
+  }
 
-    screenPosition2 = window.innerHeight / 2.8;
-    var tripPosition = tripSection.getBoundingClientRect().top;
-    if(tripPosition < screenPosition2) {
-        tripSection.classList.add('active');
-    }
-    else {
-        tripSection.classList.remove('active');
-    }
-})
+  screenPosition2 = window.innerHeight / 2.8;
+  var tripPosition = tripSection.getBoundingClientRect().top;
+  if (tripPosition < screenPosition2) {
+    tripSection.classList.add("active");
+  } else {
+    tripSection.classList.remove("active");
+  }
+});
 
-
+//ABCXYZ
