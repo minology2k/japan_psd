60c91808aefdb287d1eb6e962ca3811ae99feab6
duyensherry <dinhhueduyen@gmail.com>
2023-07-11 22:18:34 +0700

    week_11

---
+++ week_11.js
@@ -0,0 +1,15 @@
+var headerTabs = document.getElementsByClassName('header__nav-list')
+var headerIcon1 = document.querySelectorAll('.header__social > i')
+var headerLogo = document.querySelector('.logo:first-child')
+var showcaseBackground = document.getElementsByClassName('banner__overlay');
+var scrollDownButton = document.getElementsByClassName('banner__button')
+var scrollUpButton = document.getElementsByClassName('to-header')
+var categoryTabs = document.getElementsByClassName('category__list')
+var cardImage1 = document.querySelector('.category__card-img img')
+var cardImage2 = document.querySelector('.category__card-img:nth-child(2) img')
+// var cardImage3 = document.querySelector('.category__card-img:nth-child(3) img')
+// var cardImage4 = document.querySelector('.category__card-img:nth-child(4) img')
+
+
+console.log(headerTabs, headerIcon1, headerLogo, showcaseBackground, scrollDownButton, scrollUpButton, categoryTabs, cardImage1, cardImage2)
+// , cardImage3, cardImage4
